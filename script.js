b8b8cca1e584eb9f88b09e96166c2a8472fde086
mudimurtala <ibnmuhyideen95@gmail.com>
2024-07-14 14:27:03 +0100

    Create grids and apply the hover effect on them

---
+++ script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector("#container");
+
+// This code below set up my html id="container" with Flexbox
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.width = "600px"; 
+container.style.height = "600px"; 
+
+// This one below create the grid items and apply the hover effect to them
+for (let i = 0; i < 16 * 16; i++) {
+  const gridItem = document.createElement("div");
+
+  gridItem.style.backgroundColor = "white";
+  gridItem.style.border = "1px solid black";
+  gridItem.style.boxSizing = "border-box";
+  gridItem.style.flex = "1 0 calc(100% / 16)"; 
+  gridItem.style.aspectRatio = "1"; 
+
+  gridItem.addEventListener('mouseover', () => changeColor(gridItem));
+  gridItem.addEventListener('mouseout', () => restoreColor(gridItem));
+
+  container.appendChild(gridItem);
+}
+
+// This two functions below changes and restores the grid color when hovering on them
+function changeColor(element) {
+  element.style.backgroundColor = 'black';
+}
+
+function restoreColor(element) {
+  element.style.backgroundColor = 'white';
+}
