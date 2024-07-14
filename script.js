46f8fbd8562c3d1c9088b1be7b0ef0884ad03b47
mudimurtala <ibnmuhyideen95@gmail.com>
2024-07-14 17:35:40 +0100

    Make grits to be created dynamically

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
+// This code create a button on top of my webpage
+const button = document.querySelector(".btn");
+button.addEventListener("click", displayPopup);
+
+// This function take in the number input by the user
+function displayPopup() {
+  const gridSize = parseInt(prompt("Please enter the number of squares you want for your grid (maximum 100):"));
+  return (isNaN(gridSize) || gridSize <= 0 || gridSize > 100) ? alert("Please enter a valid number between 1 and 100.") : createGrid(gridSize);
+}
+  
+
 const container = document.querySelector("#container");
 
-// This code below set up my html id="container" with Flexbox
-container.style.display = "flex";
-container.style.flexWrap = "wrap";
-container.style.width = "600px"; 
-container.style.height = "600px"; 
+// The function below create my Grids dynamically when it is called
+function createGrid(size) {
+  container.innerHTML = '';
 
-// This one below create the grid items and apply the hover effect to them
-for (let i = 0; i < 16 * 16; i++) {
-  const gridItem = document.createElement("div");
+  // This code below set up my html id="container" with Flexbox
+  container.style.display = "flex";
+  container.style.flexWrap = "wrap";
+  container.style.width = "600px"; 
+  container.style.height = "600px"; 
 
-  gridItem.style.backgroundColor = "white";
-  gridItem.style.border = "1px solid black";
-  gridItem.style.boxSizing = "border-box";
-  gridItem.style.flex = "1 0 calc(100% / 16)"; 
-  gridItem.style.aspectRatio = "1"; 
+  // This one below create the grid items and apply the hover effect to them
+  for (let i = 0; i < size * size; i++) {
+    const gridItem = document.createElement("div");
 
-  gridItem.addEventListener('mouseover', () => changeColor(gridItem));
-  gridItem.addEventListener('mouseout', () => restoreColor(gridItem));
+    gridItem.style.backgroundColor = "white";
+    gridItem.style.border = "1px solid black";
+    gridItem.style.boxSizing = "border-box";
+    gridItem.style.flex = "1 0 calc(100% / 16)"; 
+    gridItem.style.aspectRatio = "1"; 
 
-  container.appendChild(gridItem);
+    gridItem.addEventListener('mouseover', () => changeColor(gridItem));
+    gridItem.addEventListener('mouseout', () => restoreColor(gridItem));
+
+    container.appendChild(gridItem);
+  }
 }
+
 
 // This two functions below changes and restores the grid color when hovering on them
 function changeColor(element) {
@@ -31,9 +48,4 @@ function restoreColor(element) {
   element.style.backgroundColor = 'white';
 }
 
-
-const btn = document.createElement("button");
-container.insertBefore(btn, container);
-btn.classList.add("myButton");
-btn.textContent = "Click Me";
 
